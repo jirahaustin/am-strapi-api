4519cb05df1068e9d33bd8b45101dd538425226c
jirahaustin <80256572+jirahaustin@users.noreply.github.com>
2022-12-26 23:57:11 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'api'),
+      host: env('DATABASE_HOST', 'containers-us-west-159.railway.app'),
+      port: env.int('DATABASE_PORT', 5868),
+      database: env('DATABASE_NAME', 'railway'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'jirahmae'),
+      password: env('DATABASE_PASSWORD', 'MJg47tXe8ULRMuNCBr9h'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
